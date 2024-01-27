2b45d1003bf9f7a7d249c6ddaba2197963e0ff18
Mendes28283488 <122620014+Mendes28283488@users.noreply.github.com>
2024-01-27 13:56:30 +0200

    0.3

--- Project2_B.py
+++ Project2_B.py
@@ -13,31 +13,6 @@ FPS = 60
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping Pong Game")
 
-#Classes:
-
-class Paddle(pygame.sprite.Sprite):
-    def __init__(self, x, y, speed):
-        super().__init__()
-        self.image = pygame.Surface((20, 100))
-        self.image.fill(WHITE)
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.speed = speed
-
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] and self.rect.top > 0:
-            self.rect.y -= 5
-        if keys[pygame.K_DOWN] and self.rect.bottom < HEIGHT:
-            self.rect.y += 5
-
-#Groups:
-            
-paddles = pygame.sprite.Group()
-ball_group = pygame.sprite.Group()
-
-player_paddle = Paddle(770, 250, 5)
-
-paddles.add(player_paddle)
 
 while True:
     for event in pygame.event.get():
@@ -45,11 +20,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    paddles.update()
-
     screen.fill(BLACK)
-    paddles.draw(screen)
-    ball_group.draw(screen)
 
     pygame.display.flip()
 
